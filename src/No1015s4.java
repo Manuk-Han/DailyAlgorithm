516c95bdd91376764c2fe1d1c36cffd48b7b5c0f
co9514 <psvm9514@gmail.com>
2022-04-12 23:30:21 +0900

    day1 수열 정렬

--- src/No1015s4.java
+++ src/No1015s4.java
@@ -1,3 +1,9 @@
+/******************************************
+ 2022-04-12 (Thu)
+ 번호 : 1015
+ 문제 : 수열 정렬
+ ******************************************/
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -5,8 +11,8 @@ public class No1015s4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int arraySize = scanner.nextInt();
-        int[] array = new int[arraySize], sortedArray = new int[arraySize];
+        int arraySize = scanner.nextInt(); //배열 크기 입력
+        int[] array = new int[arraySize], sortedArray = new int[arraySize]; // 입력 그대로 배열, 정렬할 배열 생성
 
         for( int i = 0 ; i < arraySize ; i++ ){
             int input = scanner.nextInt();
@@ -16,7 +22,7 @@ public class No1015s4 {
         Arrays.sort(sortedArray);
 
         int[] arrayP = new int[arraySize];
-        for( int i = 0 ; i < arraySize ; i++ ){
+        for( int i = 0 ; i < arraySize ; i++ ){ // 정렬한 배열값과 원상태의 배열의 값이 일치하면 수열 P에 정렬한 배열의 인덱스 삽입
             for ( int j = 0 ; j < arraySize ; j++ ){
                 if(sortedArray[i] == array[j]){
                     array[j] = 0;
@@ -29,3 +35,5 @@ public class No1015s4 {
         for( int i : arrayP) System.out.print(i + " ");
     }
 }
+
+//TODO : 맵으로 처리가 가능해 보임
