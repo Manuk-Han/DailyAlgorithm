ef4d965c4cb73f7d13a4e9ec11ab52ac89050450
Manuk-Han <aksdnr507@gmail.com>
2022-04-28 20:45:55 +0900

    day14

--- src/No1924b1.java
+++ src/No1924b1.java
@@ -1,7 +1,7 @@
 /******************************************
  2022-04-28 (Thu)
  번호 : 1924
- 문제 : 2007년
+ 문제 : 2007
  ******************************************/
 
 import java.util.Scanner;
@@ -9,8 +9,8 @@ import java.util.Scanner;
 public class No1924b1 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        int m[]= {0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5};
-        String d[]= {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        int m[] = {0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5};
+        String d[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 
         System.out.print(d[(m[scanner.nextInt() - 1] + scanner.nextInt()) % 7]);
     }
