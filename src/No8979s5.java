792da9b2682a3cbf650e2594e2b3815a460581f7
Manuk-Han <aksdnr507@gmail.com>
2022-05-04 13:54:08 +0900

    day15

--- src/No8979s5.java
+++ src/No8979s5.java
@@ -1,18 +1,43 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class No8979s5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int count = 5;
-        int hap = 0;
-        switch (count++){
-            case 3: hap+=count;
-            case 4 : hap+=count;
-            case 5: hap+=count;
-            default:hap+=count;
+        int N = scanner.nextInt(), K = scanner.nextInt();
+        HashMap<Integer, Integer> gold = new HashMap<>();
+        HashMap<Integer, Integer> silver = new HashMap<>();
+        HashMap<Integer, Integer> bronze = new HashMap<>();
+
+        for(int i = 0 ; i < N ; i++){
+            int country = scanner.nextInt();
+
+            gold.put(country, scanner.nextInt());
+            silver.put(country, scanner.nextInt());
+            bronze.put(country, scanner.nextInt());
         }
-        System.out.println(hap);
+
+        int rank = 1;
+        int[] winner = new int[N];
+
+        for(int i = 1 ; i <= N ; i++) {
+            if(gold.get(i) > gold.get(K)) winner[i-1] = 2;
+            if(gold.get(i) == gold.get(K)) winner[i-1] = 1;
+        }
+
+        for(int i = 1 ; i <= N ; i++){
+            if(winner[i-1] != 1) continue;
+            if(silver.get(i) > silver.get(K)) winner[i-1] = 2;
+            else if(silver.get(i) == silver.get(K)) winner[i-1] = 1;
+            else winner[i-1] = 0;
+        }
+
+        for(int i = 1 ; i <= N ; i++) {
+            if(winner[i-1] != 1) continue;
+            if(winner[i-1] == 1 & bronze.get(i) > bronze.get(K)) winner[i-1] = 2;
+        }
+
+        for(int i = 0 ; i < N ; i++) if(winner[i] == 2) rank++;
+        System.out.print(rank);
     }
 }
