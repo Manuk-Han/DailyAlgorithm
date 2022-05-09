1f13af25d374f6d6eb28e5893476d32b1f5083c2
Manuk-Han <aksdnr507@gmail.com>
2022-05-09 14:49:32 +0900

    day16

---
+++ src/No10926b5.java
@@ -0,0 +1,13 @@
+/******************************************
+ 2022-05-09 (Mon)
+ 번호 : 10926
+ 문제 : ??!
+ ******************************************/
+
+import java.util.Scanner;
+
+public class No10926b5 {
+    public static void main(String[] args) {
+        System.out.print(new Scanner(System.in).next() + "??!");
+    }
+}
---
+++ src/No18108b5.java
@@ -0,0 +1,13 @@
+/******************************************
+ 2022-05-09 (Mon)
+ 번호 : 18108
+ 문제 : 1998년생인 내가 태국에서는 2541년생?!
+ ******************************************/
+
+import java.util.Scanner;
+
+public class No18108b5 {
+    public static void main(String[] args) {
+        System.out.print(new Scanner(System.in).nextInt()-543);
+    }
+}
