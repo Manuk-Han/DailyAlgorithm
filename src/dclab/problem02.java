a40c2f9b27e7d9169d09fcdfde7b4db84202d4f9
Manuk-Han <aksdnr507@gmail.com>
2023-10-27 18:08:58 +0900

    test

--- src/dclab/problem02.java
+++ src/dclab/problem02.java
@@ -1,69 +1,24 @@
-/**
- 좌표 2개를 입력 받고, 입력 받은 좌표 들과 (0,0) 좌표가 꼭짓점 으로 이루어진 삼각형의 넓이를 구해라.
-각 좌표의 x와 y 축의 값은 정수이다.
-
-조건 :
-1. 각 좌표의 (x,y)의 법위는 0 <= x, y <= 500 이다.
-2. 주어진 좌표 들로 삼각형을 만들 수 없다면 -1을 출력해라.
-3. 출력은 정수라며 정수, 유리수이고 소수점 5자리 이상이라면 반올림 하시오.
-
-입력 1
-1 3
-2 4
-
-출력 1
-7.33333
-
-입력 2
-123 321
-41 14
-
-
-출력 2
-5719.5
-
-입력 3
-10 10
-20 20
-
-출력 3
--1
- **/
-
 package dclab;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class problem02 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.close();
+        int n = new Scanner(System.in).nextInt();
 
-        String result = convertNumberToString(n);
-        System.out.println(result);
-    }
+        String side = "*".repeat(n) + " ".repeat((n-2)*2+1) + "*".repeat(n);
 
-    public static String convertNumberToString(int n) {
-        if (n == 0) {
-            return "a";
-        }
+        System.out.println(side);
 
-        StringBuilder result = new StringBuilder();
+        for (int i = 1; i < n-1; i++)
+            System.out.println(" ".repeat(i) + "*" + " ".repeat(n-2) + "*" + " ".repeat(((n-i-2)*2+1)) + "*" + " ".repeat(n-2) + "*");
 
-        while (n > 0) {
-            int remainder = n % 100;
-            if (remainder > 26) {
-                int firstDigit = remainder / 10;
-                int secondDigit = remainder % 10;
-                result.insert(0, (char) ('a' + secondDigit - 1));
-                n = firstDigit;
-            } else {
-                result.insert(0, (char) ('a' + remainder - 1));
-                n = n / 100;
-            }
-        }
+        System.out.println(" ".repeat(n-1) + "*" + " ".repeat(n-2) + "*" + " ".repeat(n-2) + "*");
 
-        return result.toString();
+        for (int i = n-2; i > 0; i--)
+            System.out.println(" ".repeat(i) + "*" + " ".repeat(n-2) + "*" + " ".repeat(((n-i-2)*2+1)) + "*" + " ".repeat(n-2) + "*");
+
+        System.out.println(side);
     }
 }
--- src/dclab/problem03.java
+++ src/dclab/problem03.java
@@ -1,28 +1,51 @@
 package dclab;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class problem03 {
+    static int problemNum = 5;
+    static String inFileName = problemNum + ".in";
+    static String outFileName = problemNum + ".out";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine(), output = "";
 
-        for(int i = 0 ; i < input.length() ; i++){
-            if(i == input.length()-1) {
-                output += (char)('a'+Integer.parseInt(input.charAt(i)+"")-1);
-                break;
-            }
-            int now = Integer.parseInt(input.substring(i, i+2));
+        try {
+            File inFile = new File("src/dclab/problem03/in/" + inFileName);
+            File outFile = new File("src/dclab/problem03/out/" + outFileName);
+            FileWriter writer = new FileWriter(inFile);
+            writer.write(input);
+            writer.close();
 
-            if(now > 0 && now < 27) {
-                output += (char)('a'+now-1);
-                i++;
-            } else if(now >= 27){
-                output += (char)('a'+Integer.parseInt(input.charAt(i)+"")-1);
+            for (int i = 0; i < input.length(); i++) {
+                if (i == input.length() - 1) {
+                    if(input.charAt(i) == '0') break;
+
+                    output += (char) ('a' + Integer.parseInt(input.charAt(i) + "") - 1);
+                    break;
+                }
+                int now = Integer.parseInt(input.substring(i, i + 2));
+
+                if (now > 0 && now < 27) {
+                    output += (char) ('a' + now - 1);
+                    i++;
+                } else if (now >= 27) {
+                    output += (char) ('a' + Integer.parseInt(input.charAt(i) + "") - 1);
+                }
             }
+
+            writer = new FileWriter(outFile);
+            writer.write(output);
+            writer.close();
+
+            System.out.println(output);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        System.out.println(output);
     }
 }
