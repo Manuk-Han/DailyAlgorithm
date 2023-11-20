2a2e5b8c3efe9ba17b32b4f81e6a4c7f35bb5bfc
Manuk-Han <aksdnr507@gmail.com>
2023-11-20 20:00:22 +0900

    max & min

--- src/No2491s4.java
+++ src/No2491s4.java
@@ -4,22 +4,19 @@ public class No2491s4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int num = scanner.nextInt();
-        int globalMinCount = 1, globalMaxCount = 1, localMinCount = 1, localMaxCount = 1, min, max;
-        min = max = scanner.nextInt();
+        int num = scanner.nextInt(), now = scanner.nextInt();
+        int count = 1, min = 1, max = 1;
 
         for(int i = 0 ; i < num-1 ; i++){
             int input = scanner.nextInt();
 
-            localMinCount = input > min ? 1 : localMinCount+1;
-            globalMinCount = Math.max(globalMinCount, localMinCount);
-            min = input;
+            min = input > now ? 1 : min+1;
+            max = input < now ? 1 : max+1;
+            now = input;
 
-            localMaxCount = input < max ? 1 : localMaxCount+1;
-            globalMaxCount = Math.max(globalMaxCount, localMaxCount);
-            max = input;
+            count = Math.max(count, Math.max(min, max));
         }
 
-        System.out.println(Math.max(globalMinCount, globalMaxCount));
+        System.out.println(count);
     }
 }
