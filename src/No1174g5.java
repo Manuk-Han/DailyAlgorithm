9c505ad683a97487e61d882b3f2c4234eccc3235
Manuk-Han <aksdnr507@gmail.com>
2023-11-25 16:32:50 +0900

    digit

---
+++ src/No1174g5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class No1174g5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        boolean[] check = new boolean[10];
+
+
+    }
+
+    static void find(boolean[] arr, int num) {
+
+    }
+}
