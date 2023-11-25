23270f4fbc4842f64ada5de0bb2f740466b15f0e
Manuk-Han <aksdnr507@gmail.com>
2023-11-25 16:49:46 +0900

    stack

--- src/No1174g5.java
+++ src/No1174g5.java
@@ -1,17 +1,48 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class No1174g5 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static Map<Integer, Long> map = new HashMap<>();
+    static int N;
+    static int[] result;
+    static Stack<Long> stack;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= 10; i++){
+            map.put(i, (long)(i - 1));
+        }
+        int start = 11;
+        for(int i = 2; i <= 10; i++){
+            //i : 자리수
 
-        int n = scanner.nextInt();
+            result = new int[i];
+            stack = new Stack<>();
+            recursive(i, 0, 9);
 
-        boolean[] check = new boolean[10];
-
-
+            while(!stack.isEmpty())
+                map.put(start++, stack.pop());
+        }
+        if(N > map.size()){
+            System.out.println(-1);
+        }else{
+            System.out.println(map.get(N));
+        }
     }
+    static void recursive(int M, int m, int start){
+        if(m == M){
+            StringBuilder sb = new StringBuilder();
+            for (int i : result) {
+                sb.append(i);
+            }
+            stack.push(Long.parseLong(sb.toString()));
 
-    static void find(boolean[] arr, int num) {
+            return;
+        }
+        for(int i = start; i >= 0; i--){
+            result[m] = i;
+            recursive(M, m + 1, i - 1);
+        }
 
     }
 }
