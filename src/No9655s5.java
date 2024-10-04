6dd2d84c26c02461e335b9b6b81d050e52b166ef
Manuk-Han <aksdnr507@gmail.com>
2024-10-04 22:25:53 +0900

    .

---
+++ src/No9655s5.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class No9655s5 {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).nextInt() % 2 == 0 ? "CY" : "SK");
+    }
+}
